1396f58e08b99aa951c6d91e2e4284c586ef8135
M0hanad1 <mohned.dabsha@gmail.com>
2023-12-15 18:11:56 +0200

    Moved to TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-900 text-cyan-500">
+      <h1 className="text-7xl font-bold">Mohanad</h1>
+    </div>
+  );
+}
