130817748968d62d15ecf3669c24f2b4f1bb8b7b
M0hanad1 <mohned.dabsha@gmail.com>
2023-12-22 23:24:41 +0200

    updated container media queries

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -10,7 +10,12 @@ export default function Container({
   className = "",
 }: ContainerProps) {
   return (
-    <div className={"mx-auto w-full px-4 md:w-[98%] " + className}>
+    <div
+      className={
+        "mx-auto w-full px-4 md:w-[750px] lg:w-[1000px] xl:w-[1250px] " +
+        className
+      }
+    >
       {children}
     </div>
   );
--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -4,7 +4,7 @@ import NavLink from "./NavLink";
 export default function Header() {
   return (
     <div className="fixed inset-0 h-16 bg-black/30 font-bold shadow-md backdrop-blur-sm">
-      <Container className="flex h-full items-center justify-center gap-20">
+      <Container className="flex h-full items-center justify-evenly">
         <NavLink link="#" name="home" />
         <NavLink link="#" name="skills" />
         <NavLink link="#" name="projects" />
